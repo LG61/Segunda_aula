3c7d6f8801e786cb6f8c8a9dbbb414f680806871
LG61 <luancordeiro732@gmail.com>
2021-09-02 15:44:49 -0300

    text 1

--- bin/segunda_aula.dart
+++ bin/segunda_aula.dart
@@ -2,5 +2,13 @@ import 'package:segunda_aula/segunda_aula.dart' as segunda_aula;
 import 'dart:io';
 
 void main(List<String> arguments) {
-
+ var cont = 0;
+ var lista = <String>[];
+ print('digita ai:');
+ while(cont < 4){
+ var nome = stdin.readLineSync();
+ lista.insert(cont, nome!);
+ cont++;
+}
+print(lista);
 }
